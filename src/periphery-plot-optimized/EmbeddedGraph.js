5fdba8e690ae83473efb40ab7a406f53b0df7410
Michael Kintscher <michael@kintscher.dev>
2026-04-13 15:54:18 -0700

    Added the Delaunay triangulation from D3 to the embedded graph structure.

--- src/periphery-plot-optimized/EmbeddedGraph.js
+++ src/periphery-plot-optimized/EmbeddedGraph.js
@@ -1,5 +1,14 @@
 //
 // ***********************************************************************************
+//                      IMPORTS
+//
+// ***********************************************************************************
+//
+
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+//
+// ***********************************************************************************
 //                      CLASS DEFINITION
 //
 // ***********************************************************************************
@@ -11,12 +20,17 @@
 class EmbeddedGraph {
 
     // Properties
-    nodeMap;
+    nodeMap;        // This is a hash map that maps node IDs to their corresponding node objects.
+    delaunay;       // This is the Delaunay triangulation of the graph's nodes, which serves as an acceleration structure for geometric queries.
+    delaunayToNodeMap;  // This is a hash map that maps Delaunay indices to their corresponding node IDs.
+    nodeToDelaunayMap;  // This is a hash map that maps node IDs to their corresponding Delaunay indices.
 
     constructor(edgeList) {
 
-        // Initialize the node map.
+        // Initialize the node and Delaunay ID maps.
         this.nodeMap = new Map();
+        this.delaunayToNodeMap = new Map();
+        this.nodeToDelaunayMap = new Map();
 
         // Rebuild the acceleration structure.
         this.rebuild(edgeList);
@@ -31,6 +45,9 @@ class EmbeddedGraph {
 
     rebuild(edgeList) {
 
+        // Initialize the flat array for input to the Delaunay triangulation.
+        let points = [];
+
         // Build the adjacency list.
         // This is a hash map that maps node IDs to their adjacency lists.
         this.nodeMap.clear();
@@ -40,15 +57,24 @@ class EmbeddedGraph {
             let sourceId = edge.source;
             let targetId = edge.target;
 
-            // Reference the edge's source and target nodes' coordinates.
-            let sourceX = edge.sourceX;
-            let sourceY = edge.sourceY;
-            let targetX = edge.targetX;
-            let targetY = edge.targetY;
+            // Reference the edge's source and target nodes' coordinates, and convert them to numbers.
+            let sourceX = +edge.sourceX;
+            let sourceY = +edge.sourceY;
+            let targetX = +edge.targetX;
+            let targetY = +edge.targetY;
 
             // If the map does not already contain the node, map the node by its ID.
             if (!this.nodeMap.has(sourceId)) {
                 this.nodeMap.set(sourceId, new Node(sourceId, sourceX, sourceY));
+
+                // Add the source node's coordinates to the flat array for input to the Delaunay triangulation.
+                points.push(sourceX, sourceY);
+
+                // Map the Delaunay index to the node ID and vice versa.
+                let delaunayIndex = (points.length / 2) - 1;
+                this.delaunayToNodeMap.set(delaunayIndex, sourceId);
+                this.nodeToDelaunayMap.set(sourceId, delaunayIndex);
+
             }
 
             // Add the edge to the node's adjacency list.
@@ -57,13 +83,30 @@ class EmbeddedGraph {
             // For an undirected graph, also add the edge to the target node's adjacency list.
             if (!this.nodeMap.has(targetId)) {
                 this.nodeMap.set(targetId, new Node(targetId, targetX, targetY));
+
+                // Add the target node's coordinates to the flat array for input to the Delaunay triangulation.
+                points.push(targetX, targetY);
+
+                // Map the Delaunay index to the node ID and vice versa.
+                let delaunayIndex = (points.length / 2) - 1;
+                this.delaunayToNodeMap.set(delaunayIndex, targetId);
+                this.nodeToDelaunayMap.set(targetId, delaunayIndex);
             }
             this.nodeMap.get(targetId).topoAdjacencyList.push(sourceId);
-
         });
+
+        // Build the Delaunay triangulation from the flat array of points.
+        this.delaunay = new d3.Delaunay(points);
     }
 
 }
+
+//
+// ***********************************************************************************
+//                      SUPPORTING CLASS DEFINITION
+//
+// ***********************************************************************************
+//
 
 class Node {
 
